f9b1d8144dccc6f78913ddd63d494a5409671604
devilroop <61988264+devilroop@users.noreply.github.com>
2020-03-25 18:20:34 +0530

    Add files via upload

---
+++ 25 mar task.py
@@ -0,0 +1,20 @@
+#python==PytHoN
+a="python"
+print(a[0].upper()+a[1:3]+a[3].upper()+a[-2]+a[-1].upper())
+#pythonjava==PythoNJavA
+a=input("first string:")
+b=input("second string:")
+a="python"
+b="java"
+print(a[0].upper()+a[1:5]+a[-1].upper()+b[0].upper()+b[1:3]+b[-1].upper())
+#python==PythoN
+a="python"
+print(a[0]+a[1].upper()+a[2:3]+a[-2].upper()+a[5])
+#task==8
+a=input("first number")
+b=input("second number")
+a="3"*4
+b="5"*7
+print(a+b)
+
+
